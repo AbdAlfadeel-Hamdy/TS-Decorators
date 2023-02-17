515d3de1b562f16fc0a1aaecfa5f3addb7446d0f
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2023-02-17 09:21:25 +0200

    Accessor and parameter docorators

--- src/app.ts
+++ src/app.ts
@@ -30,3 +30,55 @@ class Person {
 }
 
 const person = new Person();
+
+////////////////////////////
+
+const Log = (target: any, propertyName: string | Symbol) => {
+  console.log("Property Decorator");
+  console.log(target); // prototype of our object or it will be constructor function if dealing with static accessor / method
+  console.log(propertyName);
+};
+
+const Log2 = (
+  target: any,
+  name: string | Symbol,
+  descriptor: PropertyDescriptor
+) => {
+  console.log("Accessor Decorator");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+};
+const Log3 = (
+  target: any,
+  name: string | Symbol,
+  descriptor: PropertyDescriptor
+) => {
+  console.log("Method Decorator");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+};
+const Log4 = (target: any, name: string | Symbol, position: number) => {
+  console.log("Parameter Decorator");
+  console.log(target);
+  console.log(name);
+  console.log(position);
+};
+class Product {
+  @Log
+  title: string;
+  _price: number;
+  @Log2
+  set price(value: number) {
+    if (value > 0) this._price = value;
+  }
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
+    return this._price * (tax + 1);
+  }
+}
